953c0620d9295e29b5c62f3c8f78c1f25b2a71ac
Kochkina Ekaterina <Gudrum@yandex.ru>
2024-05-09 00:43:34 +0200

    feat: создать схемы сущностей

---
+++ utils/constants.ts
@@ -0,0 +1,5 @@
+const textAbout = 'Пока ничего не рассказал о себе';
+const defaultAvatar = 'https://i.pravatar.cc/300';
+const minLimText = 2;
+const maxLimTextUserName = 30;
+export { minLimText, textAbout, defaultAvatar, maxLimTextUserName };
